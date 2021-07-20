4d9011c27a62a55195d37f26cebe6a2e67223c2a
Natali Soares <natalisoares90@gmail.com>
2021-07-20 07:36:58 -0300

    estilizando com bootstrap

--- resources/views/series/index.php
+++ resources/views/series/index.php
@@ -6,14 +6,22 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Controle de Séries</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body>
+     <div class="container">     
+        <div class="jumbotron">
+            <h1>Series</h1>
+        </div>    
 
-    <ul>
-        <?php foreach ($series as $serie): ?>
-        <li><?= $serie; ?></li>
-        <?php endforeach; ?>
-            
-    </ul>            
+        <a class="btn btn-primary" href="#" role="button">Adicionar serie</a>
+        
+         <ul class="list-group">
+            <?php foreach ($series as $serie): ?>
+            <li class="list-group-item"><?= $serie; ?></li>
+            <?php endforeach; ?>
+
+         </ul>            
 </body>
 </html>
