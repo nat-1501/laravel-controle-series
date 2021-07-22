c43c75af64c6e49131dbe2ed78e8f361c47e3944
Natali Soares <natalisoares90@gmail.com>
2021-07-22 18:04:19 -0300

    usando blade

---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,20 @@
+
+
+extends ('layout')
+
+section('cabecalho')
+
+Séries
+
+@section('conteudo')
+
+ <a  href="/series/criar" class="btn btn-primary" role="button">Adicionar serie</a>
+        
+<ul class="list-group">
+    <?php foreach ($series as $serie): ?>
+    <li class="list-group-item"><?= $serie; ?></li>
+    <?php endforeach; ?>
+
+ </ul>            
+
+@endsection
