df6b29558e8a5e21b17b02e63d563c1904d4eac0
Natali Soares <natalisoares90@gmail.com>
2021-08-02 21:31:12 -0300

    mudando versão do projeto laravel

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,31 +1,24 @@
 <?php
 
-namespace app\Http\Controllers;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
-
 {
-
-   public function index (Request $request) {
-    
+    public function index() {
         $series = [
-
             'Grey\'s Anatomy',
             'Lost',
-            'Agente of Shield'
+            'Agents of SHIELD'
+        ];
 
-        ];         
+        $html = "<ul>";
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
 
-        return view('series.index', compact('series'));
-
-}
-
-
-public function create ()
-{
-    return view('series.create');
-}
+        return $html;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\SeriesController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/series', 'SeriesController@index');
