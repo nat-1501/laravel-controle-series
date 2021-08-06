ab1e9d6283d4e865e0bb3f764b20eb0cfcf5c428
Natali Soares <natalisoares90@gmail.com>
2021-08-05 22:08:10 -0300

    mudando versão do projeto laravel

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
 Route::get('/series', 'SeriesController@index');
+Route::get('/series/criar', 'SeriesController@create');
