92758bbcd14b6b91a7ace7b835234049e5a1fc55
Natali Soares <natalisoares90@gmail.com>
2021-08-27 19:25:45 -0300

    criando lista para temporadas

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -6,5 +6,11 @@
 @endsection
 
 @section('conteudo')
+
+    <ul class="list-group">
+        @foreach ($temporadas as $temporada)
+            <li class="list-group-item"> {{$temporada->numero}}</li>
+            
+        @endforeach
     
 @endsection
