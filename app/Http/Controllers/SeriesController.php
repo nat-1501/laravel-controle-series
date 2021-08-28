d07f1bb4730e83442d92d0081a4e0899f35c33be
Natali Soares <natalisoares90@gmail.com>
2021-08-28 13:40:14 -0300

    Criando um criador de serie para organizar as classes

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
+use App\Services\CriadordeSerie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -24,18 +25,17 @@ class SeriesController extends Controller
     }
 
 
-    public function store(SeriesFormRequest $request)
+    public function store(
+        SeriesFormRequest $request, 
+        CriadordeSerie $criadordeSerie
+        ) 
     {
 
-        $serie = Serie::create(['nome' => $request->nome]);
-         $qtdTemporadas = $request->qtd_temporadas;
-        for ($i = 1; $i <= $qtdTemporadas; $i++) {
-            $temporada =  $serie->temporadas()->create(['numero' => $i]);
-
-            for ($j = 1; $j <= $request->ep_por_temporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-        }
+        $serie = $criadordeSerie->criarSerie(
+            $request->nome,
+            $request->qtd_temporadas,
+            $request->ep_por_temporada);
+        
 
         $request->session()
             ->flash(
---
+++ app/Services/CriadordeSerie.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Serie;
+
+class CriadordeSerie
+{
+    public function criarSerie (
+        string $nomeSerie,
+        int $qtdTemporadas,
+        int $epPorTemporada
+        
+    ):Serie {
+
+        $serie = Serie::create(['nome' => $nomeSerie]);
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada =  $serie->temporadas()->create(['numero' => $i]);
+
+            for ($j = 1; $j <= $epPorTemporada; $j++) {
+                $temporada->episodios()->create(['numero' => $j]);
+            }
+    }
+        return $serie;
+    }
+
+}
