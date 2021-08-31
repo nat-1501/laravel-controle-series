c9c56b0f30c92e4abd95a009cde8ba6d66bb844c
Natali Soares <natalisoares90@gmail.com>
2021-08-30 21:00:01 -0300

    identando com private function para melhor visualização

--- app/Services/RemovedordeSerie.php
+++ app/Services/RemovedordeSerie.php
@@ -8,18 +8,14 @@ use Illuminate\Support\Facades\DB;
 class RemovedordeSerie
 {
     public function removerSerie (int $serieId): string
-{
+{       
+    $nomeSerie = '';
     DB::transaction(function () use($serieId, &$nomeSerie) {
 
-    $nomeSerie = '';
-    $serie = Serie::find($serieId);
-    $nomeSerie = $serie->nome;
-    $serie->temporadas->each(function (Temporada $temporada) {
-            $temporada->episodios->each(function(Episodio $episodio) {
-                $episodio->delete();
-            });
-            $temporada->delete();
-         });
+        $serie = Serie::find($serieId);
+        $nomeSerie = $serie->nome;
+
+        $this-> removerTemporadas($serie); 
         $serie->delete();
 
     });
@@ -28,5 +24,23 @@ class RemovedordeSerie
         return $nomeSerie;
 }
 
+    private function removerTemporadas (Serie $serie): void
+    {
+        $serie->temporadas->each(function (Temporada $temporada) {                         
+        $this->removerEpisodios($temporada);
+        $temporada->delete();
+        
+        });    
+    
+    }
+
+
+    private function removerEpisodios(Temporada $temporada): void
+
+    {
+        $temporada->episodios->each(function(Episodio $episodio) {
+            $episodio->delete();
+    });
+}
 
 }
