bc35c740347815fc14af8eb0d198ba716afd4c0b
Natali Soares <natalisoares90@gmail.com>
2021-08-31 11:06:34 -0300

    utilizando Java script para alterar o nome da serie

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -23,9 +23,23 @@ Séries
     @foreach($series as $serie)
 
     <li class="list-group-item d-flex justify-content-between align-items-center">
-        {{ $serie->nome }}
+        <span id="nome-serie-{{ $serie->id }}">{{ $serie->nome }}</span>
+
+        <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+            <input type="text" class="form-control" value="{{ $serie->nome }}">
+            <div class="input-group-append">
+                <button class="btn btn-primary" onclick="editarSerie({{ $serie->id }})">
+                    <i class="fas fa-check"></i>
+                </button>
+                @csrf
+            </div>
+        </div>
 
         <span class="d-flex">
+
+            <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{ $serie->id }})">
+                <i class="fas fa-edit"></i>
+            </button>
                 <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm mr-1"></a>
                 <i class="fas fa-external-link-alt"></i>
             
@@ -45,3 +59,17 @@ Séries
 
 </ul>
 @endsection
+
+<script>
+    function toggleInput(serieId) {
+    const nomeSerieEl = document.getElementById(`nome-serie-${serieId}`);
+    const inputSerieEl = document.getElementById(`input-nome-serie-${serieId}`);
+    if (nomeSerieEl.hasAttribute('hidden')) {
+        nomeSerieEl.removeAttribute('hidden');
+        inputSerieEl.hidden = true;
+    } else {
+        inputSerieEl.removeAttribute('hidden');
+        nomeSerieEl.hidden = true;
+    }
+}
+</script> 
