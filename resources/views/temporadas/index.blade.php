df55d5cfdcc1e8d6825b1982df5df03118525fae
Natali Soares <natalisoares90@gmail.com>
2021-09-02 17:22:05 -0300

    exibindo o numero de episodios

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -9,10 +9,16 @@
 
     <ul class="list-group">
         @foreach ($temporadas as $temporada)
-            <li class="list-group-item">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
                 <a href="">
-                    Temporada {{$temporada->numero}}</li>
+                    Temporada {{$temporada->numero}}
                 </a>    
+            <span class="badge badge-secondary"> 
+              0 /  {{ $temporada->episodios->count() }}
+            </span>    
+        
+        </li>        
+
         @endforeach
     
 @endsection
