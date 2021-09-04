93c94ab114a008f86eb22e23a098f6af2db991e4
Natali Soares <natalisoares90@gmail.com>
2021-09-04 19:58:23 -0300

    criando botão salvar

--- resources/views/episodios/index.blade.php
+++ resources/views/episodios/index.blade.php
@@ -5,13 +5,17 @@
 @endsection
 
 @section('conteudo')
-    <ul class="list-group">
-        @foreach($episodios as $episodio)
-            <li class="list-group-item d-flex justify-content-between">
-                Episódio {{ $episodio->numero }}
-                <input type="checkbox">
-            </li>
-        @endforeach    
-    </ul>
+    <form action="">
+        <ul class="list-group">
+            @foreach($episodios as $episodio)
+                <li class="list-group-item d-flex justify-content-between">
+                    Episódio {{ $episodio->numero }}
+                    <input type="checkbox">
+                </li>
+            @endforeach    
+        </ul>
+
+         <button class="btn btn-primary mt-2 mb-2">Salvar</button>
+    </form> 
 
 @endsection
