82a1ec83380eeee83b17b9ea0a6cf0960e114d7f
Natali Soares <natalisoares90@gmail.com>
2021-09-14 20:58:31 -0300

    Utilizando ferramenta de Teste

---
+++ tests/Unit/TemporadaTet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Episodio;
+use App\Temporada;
+use PHPUnit\Framework\TestCase;
+
+class TemporadaTet extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $temporada = new Temporada();
+        $episodio1 = new Episodio();
+        $episodio2 = new Episodio();
+        $episodio3 = new Episodio()
+    }
+}
