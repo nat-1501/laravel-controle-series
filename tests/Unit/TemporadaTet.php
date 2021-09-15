c5cb0db698d360d47517dfb0f3f9f6cf809d25a2
Natali Soares <natalisoares90@gmail.com>
2021-09-14 22:17:28 -0300

    Realizando testes automatizados em episodios

--- tests/Unit/TemporadaTet.php
+++ tests/Unit/TemporadaTet.php
@@ -13,11 +13,22 @@ class TemporadaTet extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $temporada = new Temporada();
-        $episodio1 = new Episodio();
-        $episodio2 = new Episodio();
-        $episodio3 = new Episodio()
-    }
+
+        public function testExample()
+{
+    $temporada = new Temporada();
+    $episodio1 = new Episodio();
+    $episodio1->assistido = true;
+    $episodio2 = new Episodio();
+    $episodio2->assistido = false;
+    $episodio3 = new Episodio();
+    $episodio3->assistido = true;
+    $temporada->episodios->add($episodio1);
+    $temporada->episodios->add($episodio2);
+    $temporada->episodios->add($episodio3);
+
+
+   $episodiosAssistidos =  $temporada->getEpisodiosAssistidos();
+   $this->assertCount(2, $episodiosAssistidos);
 }
+    }
